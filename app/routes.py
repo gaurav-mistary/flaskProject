72e1b156f61305e3066dbbbd46c3c55d62849d15
hemingw4y <gm179gaurav@gmail.com>
2020-08-20 20:43:25 +0530

    Templating

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World'
+    user = {'username' : 'Gaurav'}
+    
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body'  : 'Beautiful Day in Portland'
+        },
+        {
+            'author': {'username' : 'Susan'},
+            'body'  : 'The avengers movie was so cool'   
+        }
+    ]
+    
+    return render_template('index.html', title='Home', user=user, posts=posts)
